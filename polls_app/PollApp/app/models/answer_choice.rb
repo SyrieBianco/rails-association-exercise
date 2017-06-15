ef149424fdba9c100f45b0e46ac144206ffd8521
App Academy Student <student@appacademy.io>
2017-06-15 14:14:50 -0700

    finished models

--- polls_app/PollApp/app/models/answer_choice.rb
+++ polls_app/PollApp/app/models/answer_choice.rb
@@ -1,2 +1,15 @@
 class AnswerChoice < ApplicationRecord
+
+  validates :question_id, :answer_choice, presence: true
+
+  belongs_to :question,
+    primary_key: :id,
+    foreign_key: :question_id,
+    class_name: :Question
+
+  has_many :responses,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: :AnswerChoice
+
 end
--- polls_app/PollApp/app/models/response.rb
+++ polls_app/PollApp/app/models/response.rb
@@ -1,2 +1,15 @@
 class Response < ApplicationRecord
+
+  validates :user_id, :answer_choice_id, presence: true
+
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: :AnswerChoice
+
+  belongs_to :respondent,
+    primary_key: :id,
+    foreign_key: :user_id, 
+    class_name: :User
+
 end
